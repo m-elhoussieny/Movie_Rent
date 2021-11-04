fbbb53cfdbd050eb2199b5b8609188b6d511d24e
EngineerMohamedElhoussieny <mohamedelhoussieny110@gmail.com>
2021-11-04 17:20:08 +0200

    first commit

---
+++ Movie_Rent/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Movie_Rent.Startup))]
+namespace Movie_Rent
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
